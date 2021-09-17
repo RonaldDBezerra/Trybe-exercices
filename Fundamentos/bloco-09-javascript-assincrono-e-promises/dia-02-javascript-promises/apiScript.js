66bdcd493894b918ac5c64a9bbe13bd1db87eb3d
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-09-16 21:00:08 -0300

    adiciona dia 02

---
+++ Fundamentos/bloco-09-javascript-assincrono-e-promises/dia-02-javascript-promises/apiScript.js
@@ -0,0 +1,32 @@
+// apiScript.js
+// apiScript.js     
+const API_URL = 'https://icanhazdadjoke.com/';
+let h2 = document.getElementById("jokeContainer")
+
+const colocaNoId = (param) => {
+  const resultado = h2.innerText = param;
+  return resultado;
+}
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => data.joke)
+    .then(retorno => {
+      let h2 = document.getElementById("jokeContainer")
+      
+      const colocaNoId = (param) => {
+        const resultado = h2.innerText = param;
+        return resultado;
+      }
+      
+      colocaNoId(retorno)
+    })
+};
+
+window.onload = () => fetchJoke()
